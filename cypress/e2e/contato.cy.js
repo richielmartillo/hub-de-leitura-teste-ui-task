368e3c198df711f3c63edd62955e940d71ecd951
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-02-16 11:54:00 -0300

    automaçao do contato

--- cypress/e2e/contato.cy.js
+++ cypress/e2e/contato.cy.js
@@ -1,21 +1,58 @@
 /// <reference types="cypress"/>
 
+beforeEach(() => {
+    cy.visit('index.html')
+  
+});
+
 describe('Funcionalidade Contato', () => {
-  it.only('Deve preencher formulário de contato com sucesso', () => {
-    cy.visit('http://localhost:3000/index.html')
-  cy.get('[name="name"]').type('Richard Marlon Balestrim')
-  cy.get('[name="email"]').type('jackswalou@gmail.com')
-  cy.get('[name="subject"]').select('Sugestões')
-  cy.get('[name="message"]').type( 'Mensnagem de teste')
-  cy.get('#btn-submit').click()
-  cy.contains('Contato enviado com sucesso!').should('exist')
+  it('Deve preencher formulário de contato com sucesso', () => {
+    cy.get('[name="name"]').type('Richard Marlon Balestrim')
+    cy.get('[name="email"]').type('jackswalou@gmail.com')
+    cy.get('[name="subject"]').select('Sugestões')
+    cy.get('[name="message"]').type( 'Mensnagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Contato enviado com sucesso!').should('exist')
   })
 
-  it('Teste 1', () => {})
+  it('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
+    
+    cy.get('[name="email"]').type('jackswalou@gmail.com')
+    cy.get('[name="subject"]').select('Sugestões')
+    cy.get('[name="message"]').type( 'Mensnagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
 
-  it('Teste 2', () => {})
 
-  it('Teste 3', () => {})
+  })
 
-  it('Teste 4', () => {})
+  it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+    cy.get('[name="name"]').type('Richard Marlon Balestrim')
+    
+    cy.get('[name="subject"]').select('Sugestões')
+    cy.get('[name="message"]').type( 'Mensnagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail')
+
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem selecionar o assunto', () => {
+    cy.get('[name="name"]').type('Richard Marlon Balestrim')
+     cy.get('[name="email"]').type('jackswalou@gmail.com')
+    cy.get('[name="subject"]').clear
+    cy.get('[name="message"]').type( 'Mensnagem de teste')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto')
+  })
+
+  it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => { 
+    cy.get('[name="name"]').type('Richard Marlon Balestrim')
+    cy.get('[name="email"]').type('jackswalou@gmail.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').clear
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem')
+
+
+  })
 })
